621c7f889f966eff3dfaeddbe2826ca7f2b6dff7
bboysofus <sofusba@gmail.com>
2021-11-16 23:39:43 +0100

    Updates to title screen

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 PS = document.querySelector('#press-start')
 
+let PS_Sound = new Audio('/sounds/PS_Sound.mp3')
 
-
-const pressStart = () => {
-    if(PS.style.opacity == '100'){
+let intervalID = null
+let letCounter = 1
+let pressStartFlickering = () => {
+    if(letCounter <= 1 && PS.style.opacity == '100'){
         PS.style.opacity = '0'
     }
     else(PS.style.opacity = '100')
+
+    if(letCounter == 0){
+        clearInterval(intervalID)
+    }
 }
 
-setInterval(pressStart, 500)
+intervalID = setInterval(pressStartFlickering, 500)
+
+const pressStartPressed = () => {
+    
+}
+
+window.addEventListener("keydown", event => {
+    if(event.keyCode === 13 || event.keyCode === 32){
+        letCounter = 0
+        PS_Sound.play()
+        PS.style.color = "chartreuse"
+        PS.style.transition = 'all 1s'
+        PS.style.opacity = '0'
+    }
+})
