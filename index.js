eeec6f54fe85bc9c0c7606212fb876ef7e794e19
bboysofus <sofusba@gmail.com>
2021-11-18 21:19:58 +0100

    Theme selecter added

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ let txt = 'Hello there! Its so very nice to meet you!' /* The text */
 let speed = 30 /* The speed/duration of the effect in milliseconds */
 let typeTid = 1700
 
+const blueIn = () => {
+    document.getElementById('theme-select1').style.transform = 'scale(105%)'
+    body.style.background = '#0a0035'
+}
+
+const blueOut = () => {
+    document.getElementById('theme-select1').style.transform = 'scale(100%)'
+    body.style.background = 'black'
+}
+
+const redIn = () => {
+    document.getElementById('theme-select2').style.transform = 'scale(105%)'
+    body.style.background = '#350000'
+}
+
+const redOut = () => {
+    document.getElementById('theme-select2').style.transform = 'scale(100%)'
+    body.style.background = 'black'
+}
+
+
+
 const typeWriter = () => {
   if (i < txt.length) {
     document.getElementById("text-box-text").innerHTML += txt.charAt(i);
@@ -32,6 +54,22 @@ const enterArrow = () => {
     document.querySelector('#enter-arrow').style.opacity = '100'
 }
 
+const themeSelectScreen = () => {
+    document.getElementById('theme-select-text').style.animation = 'fadeIn .5s ease-in'
+    document.getElementById('theme-select-text').style.animationFillMode = 'forwards'
+    document.getElementById('theme-select-parent').style.opacity = '100'
+    document.getElementById('theme-select-parent').style.zIndex = '1'
+    body.style.transition = '.2s all ease-in-out'
+    setTimeout(event => {
+        document.getElementById('theme-select1').style.animation = 'popIn .5s ease-in-out'
+        document.getElementById('theme-select1').style.animationFillMode = 'forwards'
+    }, 500)
+    setTimeout(event => {
+        document.getElementById('theme-select2').style.animation = 'popIn .5s ease-in-out'
+        document.getElementById('theme-select2').style.animationFillMode = 'forwards'
+    }, 800)
+}
+
 window.addEventListener("keyup", event => {
     if(event.keyCode === 13 || event.keyCode === 32){
         PS.style.animation = 'vanish 1.5s'
@@ -46,11 +84,11 @@ window.addEventListener("keyup", event => {
             enterArrowSound.play()
             document.querySelector('#enter-arrow').style.opacity = '0'
             document.querySelector('#text-box-text').innerHTML = ''
-            txt = 'Congratulations if this works!'
+            txt = 'Welcome to the PokéCalender!'
             typeTid = 900
             i = 0
             n = 0
-            if(txt == 'Congratulations if this works!' && n == 0){
+            if(txt == 'Welcome to the PokéCalender!' && n == 0){
                 typeWriter()
                 setTimeout(enterArrow, 0 + typeTid)
                 n = 2
@@ -61,7 +99,12 @@ window.addEventListener("keyup", event => {
             document.querySelector('#enter-arrow').style.opacity = '0'
             document.querySelector('#text-box-text').innerHTML = ''
             dialogOut()
-            body.style.background = 'red'
+            body.style.background = '#0f0f0f'
+            body.style.transition = '.5s all ease-in'
+            n = 3
+        }
+        if(n == 3){
+            setTimeout(themeSelectScreen, 700)
         }
     }
     
