f3722353e040d0ce968fdcbfe11aecd208a57715
bboysofus <sofusba@gmail.com>
2021-11-18 15:24:03 +0100

    Dialog boks

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 PS = document.querySelector('#press-start')
+body = document.querySelector('body')
 
 let PSSound = new Audio('./assets/sounds/PSSound.mp3')
+let enterArrowSound = new Audio('./assets/sounds/AButton.mp3')
 
-let intervalID = null
-let letCounter = 1
-let pressStartFlickering = () => {
-    if(letCounter <= 1 && PS.style.opacity == '100'){
-        PS.style.opacity = '0'
-    }
-    else(PS.style.opacity = '100')
+let n = 1
+let i = 0
+let txt = 'Hello there! Its so very nice to meet you!' /* The text */
+let speed = 30 /* The speed/duration of the effect in milliseconds */
+let typeTid = 1700
 
-    if(letCounter == 0){
-        clearInterval(intervalID)
-    }
+const typeWriter = () => {
+  if (i < txt.length) {
+    document.getElementById("text-box-text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
 
-intervalID = setInterval(pressStartFlickering, 500)
-
-const pressStartPressed = () => {
-    PS.style.transition = 'all 1s'
-    PS.style.visibility = 'hidden'  
+const dialog = () => {
+    document.querySelector('#text-box-parent').style.animation = 'hoverIn .1s ease-in'
+    document.querySelector('#text-box-parent').style.animationFillMode = 'forwards'
 }
 
-window.addEventListener("keydown", event => {
+const enterArrow = () => {
+    document.querySelector('#enter-arrow').style.opacity = '100'
+}
+
+window.addEventListener("keyup", event => {
     if(event.keyCode === 13 || event.keyCode === 32){
-        PS.style.transition = 'all 0s'
-        letCounter = 0
-        PSSound.play()
-        PS.style.color = "chartreuse"
-        pressStartPressed()
+        PS.style.animation = 'vanish 1.5s'
+        PS.style.animationFillMode = 'forwards'
+        if(n == 1){
+            PSSound.play()
+            setTimeout(dialog, 1500)
+            setTimeout(typeWriter, typeTid)
+            setTimeout(enterArrow, 2980)
+        }
+        if(document.querySelector('#enter-arrow').style.opacity == '100' && n == 1){
+            enterArrowSound.play()
+            document.querySelector('#enter-arrow').style.opacity = '0'
+            document.querySelector('#text-box-text').innerHTML = ''
+            txt = 'Congratulations if this works!'
+            typeTid = 900
+            i = 0
+            n = 0
+            if(txt == 'Congratulations if this works!' && n == 0){
+                typeWriter()
+                setTimeout(enterArrow, 0 + typeTid)
+                n = 2
+            }
+        }
+        if(document.querySelector('#enter-arrow').style.opacity == '100' && n == 2){
+            enterArrowSound.play()
+            document.querySelector('#enter-arrow').style.opacity = '0'
+            document.querySelector('#text-box-text').innerHTML = ''
+            body.style.background = 'red'
+        }
     }
+    
 })
-
